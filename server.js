3543f90b3d2597fc56b080357f6569f0fe70e34d
Kelsey Butler <kelseybutler@Kelseys-MacBook-Pro.local>
2017-10-14 16:27:18 -0500

    add in close and run server and update errors

--- server.js
+++ server.js
@@ -1,28 +1,28 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const mongoose = require('mongoose');
-
-mongoose.promise = global.Promise;
+const morgan = require('morgan');
 
 const {PORT, DATABASE_URL} = require('./config');
 const {Blog} = require('./models');
 
-const.app = express();
+const app = express();
+
+app.use(morgan('common'));
 app.use(bodyParser.json());
 
+mongoose.promise = global.Promise;
+
 app.get('/posts'), (req, res) => {
-  Blog.find().limit(10).then(posts => {
-    res.json({
-      blogs: blogs.map(
-        (blog) => blog.apiRepr())
-    });
+  Blog.find().then(posts => {
+    res.json(blogs.map(blog => blog.apiRepr()))
+    })
     .catch(
       err => {
         console.error(err);
         res.status(500).json({message: 'Internal server error'});
       });
   });
-};
 
 app.get('/posts/:id', (req, res) => {
   Blog.findByID(req.params.id).then(blog => res.json(blog.apiRepr()))
@@ -51,14 +51,14 @@ app.post('/posts', (req, res) => {
     .then(blog => res.status(201).json(blog.apiRepr()))
     .catch(err => {
       console.error(err);
-      res.status(500).json({message; 'Internal server error'});
+      res.status(500).json({message: 'Internal server error'});
     });
 });
 
 app.put('/posts/:id', (req, res) => {
   if(!(req.params.id && req.body.id && req.params.id === req.body.id)) {
     const message = `Request path id (${req.params.id}) and request body id ` +
-      `(${req.body.id}) must match`);
+      (`${req.body.id}) must match`);
     console.error(message);
     return res.status(400).json({message: message});
   }
@@ -77,7 +77,52 @@ app.put('/posts/:id', (req, res) => {
     .catch(err => res.status(500).json({message: 'Internal server error'}));
 });
 
-app.delete('/posts/:id', (req, res){
+app.delete('/posts/:id', (req, res) => {
   Blog.findByIdAndRemove(req.params.id).then(blog => res.status(204).end())
     .catch(err => res.status(500).json({message: 'Internal server error'}));
 });
+
+app.use('*', function(req, res) {
+  res.status(404).json({message: 'Not Found'});
+});
+
+let server;
+
+function runServer(databaseUrl=DATABASE_URL, port=PORT) {
+
+  return new Promise((resolve, reject) => {
+    mongoose.connect(databaseUrl, err => {
+      if (err) {
+        return reject(err);
+      }
+      server = app.listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve();
+      })
+      .on('error', err => {
+        mongoose.disconnect();
+        reject(err);
+      });
+    });
+  });
+}
+
+function closeServer() {
+  return mongoose.disconnect().then(() => {
+     return new Promise((resolve, reject) => {
+       console.log('Closing server');
+       server.close(err => {
+           if (err) {
+               return reject(err);
+           }
+           resolve();
+       });
+     });
+  });
+}
+
+if (require.main === module) {
+  runServer().catch(err => console.error(err));
+};
+
+module.exports = {app, runServer, closeServer};
