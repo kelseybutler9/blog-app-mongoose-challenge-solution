d8cbd5459d746bc5c45eeccd5b545c27175fb7b3
Kelsey Butler <kelseybutler@Kelseys-MacBook-Pro.local>
2017-10-14 16:10:17 -0500

    add in logic to update blog posts

--- server.js
+++ server.js
@@ -54,3 +54,25 @@ app.post('/posts', (req, res) => {
       res.status(500).json({message; 'Internal server error'});
     });
 });
+
+app.put('/posts/:id', (req, res) => {
+  if(!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+    const message = `Request path id (${req.params.id}) and request body id ` +
+      `(${req.body.id}) must match`);
+    console.error(message);
+    return res.status(400).json({message: message});
+  }
+
+  const toUpdate = {};
+  const updateableFields = ['title', 'author', 'content'];
+
+  updateableFields.forEach(field => {
+    if (field in req.body) {
+      toUpdate[field] = req.body[field];
+    }
+  });
+
+  Blog.findByIdAndUpdate(req.params.id, {$set: toUpdate})
+    .then(blog => res.status(204).end())
+    .catch(err => res.status(500).json({message: 'Internal server error'}));
+});
