a078d69942d712c3d78c194ad036c68899cd50db
Kelsey Butler <kelseybutler@Kelseys-MacBook-Pro.local>
2017-10-16 21:51:52 -0500

    finalize tests

--- test/test-blog-posts.js
+++ test/test-blog-posts.js
@@ -55,7 +55,7 @@ describe('Blogs API resource', function() {
   });
 
   describe('GET endpoint', function() {
-    it('should return all existing restaurants' function() {
+    it('should return all existing blogs' function() {
       let res;
       return chai.request(app)
         .get('/posts').then(function(_res) {
@@ -91,6 +91,72 @@ describe('Blogs API resource', function() {
     });
   });
 
-  
+  describe('POST endpoint', function() {
+    it('should add a new blog', function() {
+      const newBlog = generateBlogData();
+      return chai.request(app)
+        .post('/posts')
+        .send(newBlog)
+        .then(function(res) {
+          res.should.have.status(201);
+          res.should.be.json;
+          res.body.should.be.a('object');
+          res.body.should.include.keys(
+            'id', 'title', 'author', 'content');
+          res.body.title.should.equal(newBlog.title);
+          res.body.id.should.not.be.null;
+          res.body.author.should.equal(newBlog.author);
+          res.body.content.should.equal(newBlog.content);
+          return Blog.findById(res.body.id);
+        })
+        .then(function(blog) {
+          blog.title.should.equal(newBlog.title);
+          blog.author.should.equal(newBlog.author);
+          blog.content.should.equal(newBlog.content);
+        });
+    });
+  });
 
-})
+  describe('PUT endpoint', function() {
+    it('should update fields you send over', function() {
+      const updateData = {
+        title: 'another title',
+        content: 'more content'
+      };
+
+      return Blog
+        .findOne().then(function(blog) {
+          updateData.id = blog.id;
+          return chai.request(app)
+            .put(`/posts/${blog.id}`)
+            .send(updateData);
+        })
+        .then(function(res) {
+          res.should.have.status(204);
+
+          return Blog.findById(updateData.id);
+        })
+        .then(function(blog) {
+          blog.title.should.equal(updateData.title);
+          blog.content.should.equal(updateData.content);
+        });
+      });
+  });
+
+  describe('DELETE endpoint', function() {
+    it('delete a blog by id', function() {
+      let blog;
+      return blog.findOne().then(function(_blog) {
+          blog = _blog;
+          return chai.request(app).delete(`/posts/${blog.id}`);
+        })
+        .then(function(res) {
+          res.should.have.status(204);
+          return Blog.findById(blog.id);
+        })
+        .then(function(_blog) {
+          should.not.exist(_blog);
+        });
+    });
+  });
+});
